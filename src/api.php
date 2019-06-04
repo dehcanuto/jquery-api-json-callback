d2dc7e3ac232cd6b6edea18dcc896f529dd014b0
André Canuto <victorsantos@iMac-de-Resulttado.local>
2019-06-04 13:40:43 -0300

    JQuery getting json api callback

---
+++ src/api.php
@@ -0,0 +1,52 @@
+{
+    "data": [
+        {
+            "id": 1,
+            "name": "John Doe",
+            "title": "Developer",
+            "bio": [
+            	{
+					"genre": "male",
+					"birthdate": 1989,
+					"description": "ullam et saepe reiciendis voluptatem adipiscisit amet autem assumenda provident rerum culpaquis hic commodi nesciunt rem tenetur doloremque ipsam iurequis sunt voluptatem rerum illo velit"
+        		}
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Mary Ophen",
+            "title": "doctor",
+            "bio": [
+            	{
+					"genre": "female",
+					"birthdate": 1991,
+					"description": "ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae"
+        		}
+            ]
+        },
+        {
+            "id": 3,
+            "name": "Richard Moore",
+            "title": "Engineer",
+            "bio": [
+            	{
+					"genre": "male",
+					"birthdate": 1985,
+					"description": "dolore placeat quibusdam ea quo vitae magni quis enim qui quis quo nemo aut saepe quidem repellat excepturi ut quia sunt ut sequi eos ea sed quas"
+        		}
+            ]
+        },
+        {
+            "id": 4,
+            "name": "Betty Clear",
+            "title": "Nurse",
+            "bio": [
+            	{
+					"genre": "female",
+					"birthdate": 1980,
+					"description": "quo et expedita modi cum officia vel magni doloribus qui repudiandae vero nisi sit quos veniam quod sed accusamus veritatis error"
+        		}
+            ]
+        }
+    ]
+}
